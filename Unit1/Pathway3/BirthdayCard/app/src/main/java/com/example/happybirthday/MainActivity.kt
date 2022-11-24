a88d797674c338d58c6da41b16d18da2aced2cdc
kio2317 <kio2317@naver.com>
2022-11-24 11:12:53 +0900

    :

--- Unit1/Pathway3/BirthdayCard/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ Unit1/Pathway3/BirthdayCard/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -37,22 +37,6 @@ import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
 package com.example.happybrithday
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import com.example.happybrithday.ui.theme.HappyBrithdayTheme
 
 
 class MainActivity : ComponentActivity() {
@@ -69,7 +53,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
+//
 
 @Preview(showBackground = true)
 @Composable
@@ -92,7 +76,7 @@ fun BirthdayGreetingWithText(message : String, from : String ){
             modifier = Modifier
                 .fillMaxWidth().wrapContentWidth(Alignment.CenterHorizontally)
 //                .padding(start = 16.dp, top = 16.dp)
-
+w
         )
         // from emma
         Text(
